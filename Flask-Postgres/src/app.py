5afcde8c2ba5f147df223d0a57ec3681bb9ac07f
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2023-09-21 15:47:32 -0600

    Visualización de datos y ajustes de diseño

--- Flask-Postgres/src/app.py
+++ Flask-Postgres/src/app.py
@@ -1,39 +1,49 @@
-from flask import Flask, render_template,request,redirect,url_for
-# Se importa todo de el archivo config.py
+from flask import Flask, render_template, request, redirect, url_for
 from config import *
-app=Flask(__name__)
 
-#Variable para hacer la conexion
+app = Flask(__name__)
+
 con_bd = EstablecerConexion()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-#Ruta para guardar los datos
-@app.route('/guardar_personas', methods=['POST'])
-def agregarPersonas():
-    # Creación de un cursor para la manipulacion de la base de datos
-    '''
-    -el objeto cursor es una estructura de control para el recorrido(y potencial procesamiento)
-    de los registros del resultado de una consulta
-    '''
     cursor=con_bd.cursor()
-    nombre=request.form['nombre']
-    apellido=request.form['apellido']
-    telefono=request.form['telefono']
-    # Mostrar datos
+    sql="SELECt*FROM personas"
+    cursor.execute(sql)
+    PersonasRegistradas=cursor.fetchall()
+    #En postgres no se accede a los datos con el nombre si no con la posición de la columna
+    return render_template('index.html' ,personas=PersonasRegistradas)
+
+@app.route('/guardar_personas', methods=['POST'])
+def agregar_persona():
+    crearTablaPersonas()
+    # Recorrer y consultar con postgres para los diferentes tipos de consulta.
+    cursor = con_bd.cursor()
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    telefono = request.form['telefono']
     if nombre and apellido and telefono:
-        sql="""
-        INSERT INTO personas(nombre,apellido,telefono)
-        VALUES(%s,%s,%s)
-        """
-        # Por medio de cursor con la funcion execute que recibe dos parametros se va a ejecutar la consulta
-        cursor.execute(sql,(nombre,apellido,telefono))
+        sql = """
+        INSERT INTO personas(nombre, apellido, telefono)
+        VALUES (%s, %s, %s)
+    """
+        cursor.execute(sql, (nombre, apellido, telefono))
         con_bd.commit()
         return redirect(url_for('index'))
     else:
-        return "error en la consulta"
+        return "Error de conexión"
 
-if __name__=='__main__':
-    app.run(debug=True)
+def crearTablaPersonas():
+    cursor = con_bd.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS personas(
+        id serial NOT NULL,
+        nombre character varying(30),
+        apellido character varying(30),
+        telefono character varying(10),
+        CONSTRAINT pk_id_personas PRIMARY KEY (id)
+        );
+    """)
+    con_bd.commit()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
